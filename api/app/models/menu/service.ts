81810b6d07160e0104468c30b79c0e29611114ad
Johongirr <johongir_rahimov@bk.ru>
2023-07-10 19:48:44 +0500

    added log

--- api/app/models/menu/service.ts
+++ api/app/models/menu/service.ts
@@ -1,6 +1,5 @@
 import fs from "fs";
 import path from "path";
-import bcrypt from "bcrypt";
 import { prisma } from "../../config/prisma";
 import { IDip, Status, IMenuItem, IPiece } from "../../types/menu";
 import cloudinary, { options } from "../../config/cloudinary";
@@ -8,17 +7,6 @@ import cloudinary, { options } from "../../config/cloudinary";
 
 const MenuService = {
     async getAllDips() {
-        const hashedPassword = await bcrypt.hash("qweqweqwe", 10);
-        await prisma.user.create({
-            data: {
-                name: "Jaxongir",
-                email: "jaxongir@gmail.com",
-                phoneNumber: "+998991112211",
-                status: "ACTIVE",
-                role: "ADMIN",
-                password: hashedPassword,
-            }
-        });
         return await prisma.dip.findMany({orderBy: [{created: "desc"}]});
     },
     async getDip(id: number) {
