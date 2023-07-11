65cdc492793c8e4757ded4ffc58ead35a4f7ad29
Johongirr <johongir_rahimov@bk.ru>
2023-07-11 16:19:59 +0500

    enable admin to see customer details on order

--- client/config/socketIo.ts
+++ client/config/socketIo.ts
@@ -1,4 +1,4 @@
 import {io} from "socket.io-client";
 
-// export const socket = io("https://online-pizza-delivery-api.onrender.com");
-export const socket = io("http://localhost:8800");
+export const socket = io("https://online-pizza-delivery-api.onrender.com");
+// export const socket = io("http://localhost:8800");
