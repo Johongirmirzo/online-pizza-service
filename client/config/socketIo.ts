2fe317aa727ebf395703a83945e8d1a489e82139
Johongirr <johongir_rahimov@bk.ru>
2023-07-11 12:53:47 +0500

    add server url for socket io connection

--- client/config/socketIo.ts
+++ client/config/socketIo.ts
@@ -1,3 +1,3 @@
 import {io} from "socket.io-client";
 
-export const socket = io(process.env.SERVER_URL || "http://localhost:8800");
+export const socket = io("https://online-pizza-delivery-api.onrender.com");
